dba0627b354f0290b2a64f8dd0e6bc5ab21ce93e
Mykyta-Zalogin <nzalogin316@gmail.com>
2025-02-19 23:31:57 +0200

    Завдання №2

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+let total = 90;
+let ordered = 45;
+if (total >= ordered) {
+  console.log("Замовлення оформлено, звами зв'яжеться менеджер");
+} else {
+  console.log("На складі недостатньо товарів!");
+}
